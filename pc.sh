70e86c56bd7a2f1893057d3e46100953a998b76a
lpp1985 <409511038@qq.com>
2018-08-05 18:45:55 +0800

    123

--- pc.sh
+++ pc.sh
@@ -4,7 +4,7 @@ chmod -R 755 ./*.*
 git add . -A
 git commit -m 123
 #git push -f Homenas pc:master
-git push -f Roomnas $1:master
-git push -f USA $1:master
-git push -f GIT $1:master
-git push -f HG $1:master
+git push -f Roomnas $1:master&
+git push -f USA $1:master&
+git push -f GIT $1:master&
+git push -f HG $1:master&
