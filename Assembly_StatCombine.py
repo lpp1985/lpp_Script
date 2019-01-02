8d142165e028e0c627ab45e543ccdcb9572a4ceb
lpp1985 <409511038@qq.com>
2019-01-02 14:53:01 +0800

    123

---
+++ Assembly_StatCombine.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+from lpp import *
+all_data = glob.glob("*.stats")
+END = open( "Stats.xls",'w'  )
+i=0
+for f in all_data:
+	RAW = open(f,'rU'  )
+	if i==0:
+		i=1
+		END.write("Sample\t"+ RAW.next())
+	else:
+		RAW.next()
+		name = f.rsplit(".",1)[0]
+		END.write( name+'\t'+RAW.next() )
+
