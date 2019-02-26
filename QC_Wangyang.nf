fbe00ecd408908babfbf281d925018967d9ba58c
Your Name <you@example.com>
2019-02-26 22:40:51 +0800

    123

---
+++ QC_Wangyang.nf
@@ -0,0 +1,161 @@
+#!/home/nfs/SOFTWARE/bin/nextflow
+params.adapter = "/home/nfs/SOFTWARE/Other/scythe/illumina_adapters.fa"
+params.quality = "20"
+params.input = "./"
+qc_path = params.input+"/Qc/"
+stats_path = params.input+"/STATS/"
+
+
+Channel.fromFilePairs(params.input+'/*_{1,2}.*.gz').into {qc_plot_raw; all_file }
+
+
+
+process RAW_QualityStats{
+	
+	maxForks 50
+	input:
+		set val(sampleid),file(reads) from qc_plot_raw
+		
+	output:
+		file "*.dis" into raw_graph_stat
+		file "*.stats" into  raw_stat
+		
+	"""
+		zcat ${reads[0]} |  tee -a cache.R1.fastq | fastx_quality_stats -Q 33 -o ${sampleid}.1.dis
+		zcat ${reads[1]} |  tee -a cache.R2.fastq | fastx_quality_stats -Q 33 -o ${sampleid}.2.dis
+
+
+		Quality_Cat -1 cache.R1.fastq -2 cache.R2.fastq -n 33 -o  ${sampleid}.raw
+		
+		
+	"""
+	
+
+
+}
+
+
+process qc {
+	maxForks 50
+	publishDir "$qc_path", mode: 'copy', overwrite: true
+	input: 
+		set val(sampleid),file(reads) from all_file
+	output:
+		set val(sampleid),file("*pair{1,2}") into qc_result
+		set val(sampleid),file("*pair{1,2}") into qc_result_ForStat
+		file "*.fq.gz" into gzresult
+		
+	script:
+		"""
+		sickle pe -t sanger -n -l 100 -q ${params.quality} \
+			-f <(scythe -a ${params.adapter} <(zcat ${reads[0]}) -q sanger 2> /dev/null )\
+			-r <(scythe -a ${params.adapter} <(zcat ${reads[1]}) -q sanger 2> /dev/null )\
+			-o  ${sampleid}.pair1\
+			-p  ${sampleid}.pair2\
+			-s  ${sampleid}.fail
+		gzip -c ${sampleid}.pair1  >${sampleid}_1.fq.gz
+		gzip -c ${sampleid}.pair2  >${sampleid}_2.fq.gz
+
+		
+		"""
+		
+}
+
+process QC_STAT_PROCESS{
+	
+	maxForks 50
+
+	input:
+	
+		set val(sampleid),file(qc_pair) from qc_result_ForStat 
+		
+	output:
+
+		file "*.stats" into qc_result_stat
+		
+		
+		
+	"""	
+
+		Quality_Cat -1 ${qc_pair[0]} -2 ${qc_pair[1]} -n 33 -o  ${sampleid}.Filter
+	"""
+
+
+
+}
+
+
+
+
+
+process QC_STAT_PROCESS{
+
+	maxForks 50
+
+
+	input:
+	
+		set val(sampleid),file(qc_pair) from qc_result 
+		
+	output:
+		file "*.dis" into  qc_graph_stat
+
+		
+		
+		
+	"""	
+		cat ${qc_pair[0]} | fastx_quality_stats -Q 33 -o ${sampleid}.Filter1.dis
+		cat ${qc_pair[1]} | fastx_quality_stats -Q 33 -o ${sampleid}.Filter2.dis
+		
+	"""
+
+
+
+}
+
+
+
+
+		
+filter_stat_graph = raw_graph_stat.mix(qc_graph_stat).flatten()
+
+
+
+process QC_Plot {
+	maxForks 50
+	publishDir "$stats_path", mode: 'move', overwrite: true
+
+	input:
+		file filter_stats_fastx from filter_stat_graph
+		
+	output:
+		file "*.pdf" into qc_graph
+
+		
+	"""
+		fastx_quality_boxplot.R ${filter_stats_fastx} ${filter_stats_fastx}.qualstats.pdf&&fastx_nucleotide_distributionPer.R ${filter_stats_fastx} ${filter_stats_fastx}.nucdistr.pdf
+		
+		
+	"""
+
+		
+}
+total_stat = raw_stat.mix( qc_result_stat ).collect()
+process Qc_Report{
+	maxForks 50
+	publishDir "$stats_path", mode: 'move', overwrite: true
+	input:
+		file all_statfile from total_stat
+	
+	output:
+		file "*.tsv" into qc_report
+		"""
+			QC_ResultIntegrated.py  $all_statfile QC_Result.tsv
+			
+		"""
+	}
+
+
+
+
+
---
+++ go/src/Kmer_Creep.go
@@ -0,0 +1,222 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"os/exec"
+
+	//"fmt"
+	"lpp"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var x int
+
+var path []string
+var kmer_graph map[string]map[string]string = make(map[string]map[string]string)
+var kmer_seq map[string]string = make(map[string]string)
+var kmer_PreFilter map[string]string = make(map[string]string)
+var OUTPUT *os.File
+
+func Contains(s_list []string, node string) bool {
+	res := false
+	for _, data := range s_list {
+		if data == node {
+			res = true
+		}
+	}
+	return res
+}
+func Traverse_5(order int, node string, step int, path []string, threshold int, if_out bool) []string {
+	if Contains(path, node) {
+		return path
+	}
+	path = append(path, node)
+	kmer_PreFilter[node] = ""
+
+	//	_, ok := kmer_graph[node]
+
+	if step == threshold {
+		if len(path) > 0 && if_out {
+			OUTPUT.WriteString(">" + strings.Join(path, "; ") + "\n")
+			end_seq := ""
+			for i, name := range path {
+				new_seq, _ := kmer_seq[name]
+				if i == 0 {
+
+					end_seq = new_seq
+				} else {
+					end_seq += new_seq[*kmer-1:]
+				}
+			}
+			if order%2 == 1 {
+				end_seq = string(lpp.RevComplement([]byte(end_seq)))
+			}
+			OUTPUT.WriteString(end_seq + "\n")
+		}
+
+	} else {
+		loc := Find(path, node)
+		if len(kmer_graph[node]) > 1 {
+			step += 1
+		}
+		for son, _ := range kmer_graph[node] {
+			if len(kmer_graph[node]) > 1 {
+				path = path[:loc]
+			}
+
+			path = Traverse_5(order, son, step, path, threshold, if_out)
+		}
+
+	}
+	return path
+}
+func Find(s_list []string, node string) int {
+	j := 0
+	for i, data := range s_list {
+		if data == node {
+			j = i
+			break
+		}
+	}
+	return j + 1
+}
+
+var kmer *int
+
+func main() {
+	has_son := make(map[string]string)
+	input := flag.String("i", "./", "Input File")
+	output := flag.String("o", "StringGraph.tsv", "Output File")
+	nodes_file := flag.String("l", "KmerList.tsv", "Kmer Id list")
+	kmer = flag.Int("k", 41, "kmer")
+	threshold := flag.Int("s", 10, "Step")
+	flag.Parse()
+	raw_file := lpp.GetBlockRead(*nodes_file, "\n", false, 10000)
+	var All_list []string
+	for {
+		line, err := raw_file.Next()
+		line = bytes.TrimSpace(line)
+		All_list = append(All_list, string(line))
+		if err != nil {
+			break
+		}
+
+	}
+	RAW := lpp.Fasta{File: *input}
+	OUTPUT, _ = lpp.GetOuput(*output, 1000)
+
+	reg := regexp.MustCompile(`L\:(\S)\:(\d+)\:(\S)`)
+	reg_cov := regexp.MustCompile(`KC\:i\:(\d+)`)
+
+	for {
+		title, seq, err := RAW.Next()
+		name := string(bytes.Fields(title)[0][1:])
+		all_situation := reg.FindAllStringSubmatch(string(title), -1)
+		cov, _ := strconv.Atoi(reg_cov.FindStringSubmatch(string(title))[1])
+		if cov < 30 {
+			continue
+		}
+		if len(all_situation) == 0 {
+			continue
+		}
+		if len(all_situation) < 2 {
+			continue
+		}
+		has_son[name] = ""
+		seq = bytes.TrimSpace(seq)
+		kmer_seq[name+"+"] = string(seq)
+		kmer_seq[name+"-"] = string(lpp.RevComplement(seq))
+		if err != nil {
+			break
+		}
+	}
+	RAW = lpp.Fasta{File: *input}
+	for {
+		title, _, err := RAW.Next()
+		name := string(bytes.Fields(title)[0][1:])
+		cov, _ := strconv.Atoi(reg_cov.FindStringSubmatch(string(title))[1])
+		if cov < 30 {
+			continue
+		}
+		_, ok := has_son[name]
+		if !ok {
+			continue
+		}
+		all_situation := reg.FindAllStringSubmatch(string(title), -1)
+		for _, data := range all_situation {
+			dir := data[1]
+			sub := data[2]
+			_, ok := has_son[sub]
+			if !ok {
+				continue
+			}
+			sub_dir := data[3]
+			q := name + dir
+			s := sub + sub_dir
+			_, ok1 := kmer_graph[q]
+
+			if !ok1 {
+				kmer_graph[q] = make(map[string]string)
+
+			}
+			kmer_graph[q][s] = ""
+			if dir == "+" {
+				dir = "-"
+			} else {
+				dir = "+"
+			}
+			if sub_dir == "+" {
+				sub_dir = "-"
+			} else {
+				sub_dir = "+"
+			}
+			q = sub + sub_dir
+			s = name + dir
+			_, ok2 := kmer_graph[q]
+			if !ok2 {
+				kmer_graph[q] = make(map[string]string)
+
+			}
+			kmer_graph[q][s] = ""
+
+		}
+		if err != nil {
+			break
+		}
+	}
+	/*
+		六次搜寻，将搜到的节点进行聚类
+	*/
+	for x := 0; x <= 6; x++ {
+		CACHE, _ := lpp.GetOuput("Cache.fasta", 1000)
+		for n, node := range All_list {
+
+			// OUTPUT.WriteString(node + "\n")
+			Traverse_5(n, node, 0, path, *threshold, false)
+			_, ok := kmer_PreFilter[node]
+			if ok {
+				delete(kmer_PreFilter, node)
+			}
+
+		}
+		for name, _ := range kmer_PreFilter {
+			CACHE.WriteString(">" + name + "\n" + kmer_seq[name] + "\n")
+		}
+		CACHE.Close()
+		exec.Command(fmt.Sprintf(" cdhit-est -i %s -o %s ", "Cache.fasta", "Cluster"))
+		CLUSEROUT := lpp.Fasta{"File": "Cluster.clr"}
+		for {
+			head, cont, err := CLUSEROUT.Next()
+			if err != nil {
+				break
+			}
+		}
+		kmer_PreFilter = make(map[string]string)
+
+	}
+}
